9c4d64e9fc17fa34c8e21fd1617e2895d420edbf
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-01 19:41:57 +0200

    navigation setup + screens

---
+++ myEcommerceApp/src/navigation/RootNavigator.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Text>RootNavigator</Text>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
+
+const styles = StyleSheet.create({});
