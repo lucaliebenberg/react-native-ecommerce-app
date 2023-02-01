0685e31de5d3607100b1bc5452644c2990c31347
Luca Liebenberg <liebenbergluca@gmail.com>
2023-02-01 20:15:41 +0200

    outer folder remove

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text, View } from "react-native";
+import "react-native-gesture-handler";
+
+import RootNavigator from "./src/navigation/RootNavigator";
+
+export default function App() {
+  return (
+    <>
+      <RootNavigator />
+    </>
+  );
+}
