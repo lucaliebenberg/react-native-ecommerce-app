8875afafeb8ddc21d48d4ad255e667d6e96bad57
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-02 16:55:40 +0200

    TabNavigators added to Drawer Navigation

--- src/navigation/Drawer.tsx
+++ src/navigation/Drawer.tsx
@@ -4,6 +4,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 // import screens
 import Home from "../screens/Home";
+import Favourites from "../screens/Favourites";
+import ShoppingCart from "../screens/ShoppingCart";
 import Settings from "../screens/Settings";
 
 const Drawer = () => {
@@ -12,6 +14,8 @@ const Drawer = () => {
     <Drawer.Navigator>
       {/* Drawer screens here */}
       <Drawer.Screen name="WeBuy" component={Home} />
+      <Drawer.Screen name="Favourites" component={Favourites} />
+      <Drawer.Screen name="ShoppingCart" component={ShoppingCart} />
       <Drawer.Screen name="Settings" component={Settings} />
     </Drawer.Navigator>
   );
