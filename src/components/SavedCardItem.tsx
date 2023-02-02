0846b9b6691710f55b422f811337ef1fe80b5409
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-02-02 19:53:47 +0200

    Saved Items screen card component changed

--- src/components/SavedCardItem.tsx
+++ src/components/SavedCardItem.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
 import React from "react";
 
 // import icons
@@ -20,21 +27,12 @@ const SavedCardItem = () => {
         </View>
 
         <View style={styles.itemPriceContainer}>
-          <Text style={styles.itemPrice}>R550.00</Text>
+          <Text style={styles.itemPrice}>R500</Text>
+        </View>
 
-          <Text style={styles.itemIcons}>
-            <TouchableOpacity>
-              <Entypo name="shopping-cart" size={18} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <MaterialCommunityIcons
-                style={{ marginLeft: 10 }}
-                name="delete"
-                size={18}
-                color="black"
-              />
-            </TouchableOpacity>
-          </Text>
+        <View style={styles.itemButtonsContainer}>
+          <Button title="Edit" />
+          <Button title="Remove" color={"red"} />
         </View>
       </View>
     </View>
@@ -45,8 +43,8 @@ export default SavedCardItem;
 
 const styles = StyleSheet.create({
   container: {
-    width: 320,
-    height: 95,
+    width: 360,
+    height: 140,
     backgroundColor: "#FFF",
     borderRadius: 5,
     display: "flex",
@@ -67,27 +65,30 @@ const styles = StyleSheet.create({
     paddingLeft: 12,
   },
   postImage: {
-    height: 60,
-    width: 60,
+    height: 98,
+    width: 98,
   },
   postDetailsContainer: {
-    padding: 20,
+    // padding: 28,
+    paddingLeft: 18,
+    paddingTop: 17,
   },
   itemPriceContainer: {
     display: "flex",
     flexDirection: "row",
   },
   itemPrice: {
-    fontSize: 16,
+    fontSize: 19,
     color: "grey",
     paddingTop: 5,
   },
   itemIcons: {
-    marginLeft: 110,
+    marginLeft: 62,
     color: "black",
-    fontSize: 8,
+    fontSize: 13,
     fontWeight: "bold",
-    paddingTop: 5,
+    paddingTop: 9,
+    marginRight: 16,
   },
   itemIcon: {
     paddingLeft: 5,
@@ -100,4 +101,113 @@ const styles = StyleSheet.create({
     color: "black",
     maxWidth: 180,
   },
+  itemButtonsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+  },
 });
+
+// import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+// import React from "react";
+
+// // import icons
+// import Entypo from "react-native-vector-icons/Entypo";
+// import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+// const SavedCardItem = () => {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.postImageContainer}>
+//         <Image
+//           source={require("../../assets/shopping-item.jpg")}
+//           style={styles.postImage}
+//         />
+//       </View>
+//       <View style={styles.postDetailsContainer}>
+//         <View style={styles.postTitleContainer}>
+//           <Text style={styles.postTitle}>Men's S sweatshirt</Text>
+//         </View>
+
+//         <View style={styles.itemPriceContainer}>
+//           <Text style={styles.itemPrice}>R550.00</Text>
+
+//           <Text style={styles.itemIcons}>
+//             <TouchableOpacity>
+//               <Entypo name="shopping-cart" size={18} color="black" />
+//             </TouchableOpacity>
+//             <TouchableOpacity>
+//               <MaterialCommunityIcons
+//                 style={{ marginLeft: 10 }}
+//                 name="delete"
+//                 size={18}
+//                 color="black"
+//               />
+//             </TouchableOpacity>
+//           </Text>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default SavedCardItem;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     width: 320,
+//     height: 95,
+//     backgroundColor: "#FFF",
+//     borderRadius: 5,
+//     display: "flex",
+//     flexDirection: "row",
+//     marginTop: 16,
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 2,
+//     },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 2.44,
+
+//     elevation: 5,
+//   },
+//   postImageContainer: {
+//     paddingTop: 20,
+//     paddingLeft: 12,
+//   },
+//   postImage: {
+//     height: 60,
+//     width: 60,
+//   },
+//   postDetailsContainer: {
+//     padding: 20,
+//   },
+//   itemPriceContainer: {
+//     display: "flex",
+//     flexDirection: "row",
+//   },
+//   itemPrice: {
+//     fontSize: 16,
+//     color: "grey",
+//     paddingTop: 5,
+//   },
+//   itemIcons: {
+//     marginLeft: 110,
+//     color: "black",
+//     fontSize: 8,
+//     fontWeight: "bold",
+//     paddingTop: 5,
+//   },
+//   itemIcon: {
+//     paddingLeft: 5,
+//   },
+//   postTitleContainer: {
+//     paddingTop: 5,
+//   },
+//   postTitle: {
+//     fontSize: 16,
+//     color: "black",
+//     maxWidth: 180,
+//   },
+// });
